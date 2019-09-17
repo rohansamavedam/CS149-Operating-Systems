074d85f0ccd61d7f11305589676d1de17082023f
Rohan Samavedam <42583450+rohansamavedam@users.noreply.github.com>
2019-09-16 22:34:55 -0700

    Update fork.c

--- fork.c
+++ fork.c
@@ -1,11 +1,38 @@
+// Krishna Rohan Samavedam, CS149 SECTION 01, FALL 2019
+
 #include <stdio.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
 
+double calculateSumInSingleFile(char *fileName){
+    FILE * numbersFile = fopen(fileName, "r");
+
+    double totalSum;
+
+    if (numbersFile == NULL){
+        printf("Error: File %s Not Found\n", fileName);
+        return 0;
+    }
+
+    int numberOfConversionsProcessed;
+    numberOfConversionsProcessed = fscanf(numbersFile, "%lf", &totalSum);
+
+    while(numberOfConversionsProcessed == 1){
+        double temp;
+
+        numberOfConversionsProcessed = fscanf(numbersFile, "%lf", &temp);
+
+        if(numberOfConversionsProcessed == 1) {
+            totalSum += temp;
+        }
+    }
+
+    return totalSum;
+}
+
 int main(int argc, char **argv) {
 
-//  A loop to get all the file names from command line.
+    // Looping through the command line arguments and adding file names to an array.
     char *arr[10];
     int i = 1;
     while(i < argc){
@@ -13,38 +40,29 @@ int main(int argc, char **argv) {
         arr[i-1] = fileName;
         i++;
     }
-    
+
+    // Declaring a pipe.
     int p[2];
     pipe(p);
 
     double totalTotalSum;
 
     for(int i = 0; i < argc - 1; i++){
+
         if(fork() == 0){
-//            printf("%s\n", arr[i]);
-            double totalSum;
-            // Reading and Computing Sum Part
+            // Calling the Single File Sum method that returns the sum of elements of each file.
             char * fileName = arr[i];
-            FILE * numbersFile = fopen(fileName, "r");
-            int numberOfConversionsProcessed;
-            numberOfConversionsProcessed = fscanf(numbersFile, "%lf", &totalSum);
-            while(numberOfConversionsProcessed == 1){
-                double temp;
+            double totalSum = calculateSumInSingleFile(fileName);
 
-                numberOfConversionsProcessed = fscanf(numbersFile, "%lf", &temp);
-
-                if(numberOfConversionsProcessed == 1) {
-                    totalSum += temp;
-                }
-            }
-            printf("%lf\n", totalSum);
+            // Writing the Total Sum of the File to Parent Process.
             write(p[1], &totalSum, sizeof(totalSum));
             close(p[1]);
             exit(0);
         }
+
+        // Parent process reading the value from child.
         double val;
         read(p[0], &val, sizeof(val));
-//        printf("%lf\n", val);
         totalTotalSum = totalTotalSum + val;
     }
 
@@ -52,4 +70,3 @@ int main(int argc, char **argv) {
 
     return 0;
 }
-
